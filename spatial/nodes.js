4c5f0b1f68337bbf72e51502a85ab435e4de6f7d
André Gaul <gaul@web-yard.de>
2012-10-06 00:25:37 +0200

    only emit doc._id and doc.hostname for spatial view

--- spatial/nodes.js
+++ spatial/nodes.js
@@ -1,5 +1,11 @@
 function(doc) {
     if (doc.type=='node' && doc.longitude && doc.latitude) {
-        emit({ type: "Point", coordinates: [doc.longitude, doc.latitude] }, doc);
+        emit(
+                { type: "Point", coordinates: [doc.longitude, doc.latitude] }, 
+                {
+                    id: doc._id,
+                    hostname: doc.hostname
+                }
+            );
     }
 }
