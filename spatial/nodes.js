4d85ae63f8a6cf65b03f65b092afd663ede3c625
André Gaul <gaul@web-yard.de>
2012-11-25 20:55:29 +0100

    add antenna aperture+direction

--- spatial/nodes.js
+++ spatial/nodes.js
@@ -1,11 +1,23 @@
 function(doc) {
     if (doc.type=='node' && doc.longitude && doc.latitude) {
+        var antennas = [];
+        if (doc.interfaces) {
+            for (var i=0; i<doc.interfaces.length; i++) {
+                if (doc.interfaces[i].antenna) {
+                    antennas.push({
+                        direction: doc.interfaces[i].antenna.direction,
+                        beamH: doc.interfaces[i].antenna.beamH
+                    });
+                }
+            }
+        }
         emit(
                 { type: "Point", coordinates: [doc.longitude, doc.latitude] }, 
                 {
                     id: doc._id,
                     hostname: doc.hostname,
-                    tags: doc.tags
+                    tags: doc.tags,
+                    antennas: antennas
                 }
             );
     }
